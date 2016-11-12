838551832a583f85479f49d340b53b2562fa07a1
Alexanderlau <429095816@qq.com>
2016-11-12 23:56:21 +0800

    spider

--- Youming_game_image_spider/page.py
+++ Youming_game_image_spider/page.py
@@ -1,13 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import math
 
-page = 1
-url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(page)
-html = urlopen(url)
-bsObj = BeautifulSoup(html)
-title = bsObj.find("div",{"class":"QZlisttxt"}).findAll("p")
-for name in title:
-    txt = name.get_text()
-    csvfile = open("page.txt","a+")
-    csvfile.writelines(txt+'\n')
-    csvfile.close()
+temp = 0
+for x in range(1,300):
+    temp += x
+    url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(temp)
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html)
+    title = bsObj.find("div",{"class":"QZlisttxt"}).findAll("p")
+    for name in title:
+        txt = name.get_text()
+        csvfile = open("page.txt","a+")
+        csvfile.writelines(txt+'\n')
+        csvfile.close()
--- Youming_game_image_spider/title.py
+++ Youming_game_image_spider/title.py
@@ -1,15 +1,18 @@
 from urllib.request import urlopen,urlretrieve
 from bs4 import BeautifulSoup
+import math
 
-page = 1
-url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(page)
-html = urlopen(url)
-bsObj = BeautifulSoup(html)
-for link in bsObj.find("div", {"class": "QZlisttxt"}).findAll("a"):
-    if 'href' in link.attrs:
-        txt = link.attrs['href']
-        titleFile = open("title.txt","a+")
-        titleFile.writelines(txt + '\n')
+temp = 0
+for x in range(1,20):
+    temp += x
+    url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(temp)
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html)
+    for link in bsObj.find("div", {"class": "QZlisttxt"}).findAll("a"):
+        if 'href' in link.attrs:
+            txt = link.attrs['href']
+            titleFile = open("title.txt","a+")
+            titleFile.writelines(txt + '\n')
 
 
 
