01ed65033b078bafec181d8ca03279a5f44ecaf8
Alexanderlau <429095816@qq.com>
2016-11-13 21:26:03 +0800

    spider

--- Youming_game_image_spider/3DMgame_gl_spider.py
+++ Youming_game_image_spider/3DMgame_gl_spider.py
@@ -1,10 +1,23 @@
+#-*-coding:utf-8-*-
 from urllib.request import urlretrieve,urlopen
 from bs4 import BeautifulSoup
 import re
+import sys
 
-url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=1"
-html = urlopen(url)
-bsObj = BeautifulSoup(html)
-for link in bsObj.find("div",{"class":"QZlisttxt"}).findAll("a",href=re.compile("()")):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+temp = 0
+for x in range(1,300):
+    temp += x
+    url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(temp)
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html)
+    for link in bsObj.find("div",{"class":"QZlisttxt"}).findAll("a",href=re.compile("(/gl/)(\d)+")):
+        if 'href' in link.attrs:
+            href = link.attrs['href']
+            content = urlopen(href)
+            title = href[-12:]
+            file = open(r'gl_txt/' + title,"wb").writelines(content)
+
+
+        #http://www.3dmgame.com /gl/201611/3607132.html
+
+
