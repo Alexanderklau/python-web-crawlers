dfea1c958aaaa9eefeff97d5261ae640e02c66fe
Alexanderlau <429095816@qq.com>
2016-11-13 22:30:31 +0800

    spider

--- Youming_game_image_spider/3DMgame_gl_spider.py
+++ Youming_game_image_spider/3DMgame_gl_spider.py
@@ -3,12 +3,20 @@ from urllib.request import urlretrieve,urlopen
 from bs4 import BeautifulSoup
 import re
 import sys
+import urllib.request
+import http.cookiejar
 
 temp = 0
 for x in range(1,300):
     temp += x
     url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(temp)
-    html = urlopen(url)
+    req = urllib.request.Request(url, headers={
+        'Connection': 'Keep-Alive',
+        'Accept': 'text/html, application/xhtml+xml, */*',
+        'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
+    })
+    html = urlopen(req)
     bsObj = BeautifulSoup(html)
     for link in bsObj.find("div",{"class":"QZlisttxt"}).findAll("a",href=re.compile("(/gl/)(\d)+")):
         if 'href' in link.attrs:
--- Youming_game_image_spider/page.py
+++ Youming_game_image_spider/page.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+from urllib.request import urlopen,Request
 from bs4 import BeautifulSoup
 import math
 
@@ -6,7 +6,13 @@ temp = 0
 for x in range(1,300):
     temp += x
     url = "http://www.3dmgame.com/gl/?tid=26&totalpage=527&page=" + str(temp)
-    html = urlopen(url)
+    req = Request(url, headers={
+        'Connection': 'Keep-Alive',
+        'Accept': 'text/html, application/xhtml+xml, */*',
+        'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
+    })
+    html = urlopen(req)
     bsObj = BeautifulSoup(html)
     title = bsObj.find("div",{"class":"QZlisttxt"}).findAll("p")
     for name in title:
