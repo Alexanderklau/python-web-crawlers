81084d495d6fb6ba1dd7f4bd58c36b4fa73446a5
Alexanderlau <429095816@qq.com>
2016-11-17 12:51:14 +0800

    spider

--- Youming_game_image_spider/Game_url.py
+++ Youming_game_image_spider/Game_url.py
@@ -2,21 +2,20 @@ from urllib.request import urlretrieve,urlopen,Request
 from bs4 import BeautifulSoup
 import re
 
-temp = 0
-for x in range(1,3):
-    temp += x
-index = "index_" + str(temp)
-url = "http://pic.ali213.net/list/game/" + index + '.html'
-req = Request(url,headers={
+for page in range(2, 10):
+    Url = "http://pic.ali213.net/list/game/index_%s.html" % (page)
+    print(Url)
+    req = Request(Url, headers={
         'Connection': 'Keep-Alive',
         'Accept': 'text/html, application/xhtml+xml, */*',
         'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'})
-html = urlopen(url)
-bsObj = BeautifulSoup(html)
-for link in bsObj.findAll("a",href=re.compile("(/html/)")):
-    if 'href' in link.attrs:
-        href = link.attrs['href']
-        imageurl = "http://pic.ali213.net" + href
-        print(imageurl)
+    html = urlopen(req)
+    bsObj = BeautifulSoup(html)
+    for link in bsObj.findAll("a", href=re.compile("(/html/)")):
+        if 'href' in link.attrs:
+            href = link.attrs['href']
+            imageurl = "http://pic.ali213.net" + href
+            print(imageurl)
+
 
