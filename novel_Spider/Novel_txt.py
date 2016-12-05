da448c1ce4dfbd3aa06ee97492391bacdfc072c7
Alexanderlau <429095816@qq.com>
2016-12-05 11:10:33 +0800

    spider

--- novel_Spider/Novel_txt.py
+++ novel_Spider/Novel_txt.py
@@ -2,13 +2,21 @@
 from urllib.request import *
 import re
 from bs4 import BeautifulSoup
+import time
 
 url = 'http://www.quanshu.net/'
 req = urlopen(url)
 bsObj = BeautifulSoup(req)
-for i in bsObj.find("li").findAll("a",href=re.compile("(book)")):
+for i in bsObj.findAll("a",href=re.compile("(book_\d)")):
     if 'href' in i.attrs:
         href = i.attrs['href']
-        print(href)
+        txtUrl = urlopen(href)
+        UrlObj = BeautifulSoup(txtUrl)
+        for x in UrlObj.findAll("a",href=re.compile("(/book/\d/\d)$")):
+            if 'href' in x.attrs:
+                urlef = x.attrs['href']
+                print(urlef)
+                txt =open("url.txt","a+").writelines(urlef + '\n')
+
 
 
