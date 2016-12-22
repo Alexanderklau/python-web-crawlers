bb10067011f43715122ddc3fdc7bfb6a1a1dc8ba
Alexanderlau <429095816@qq.com>
2016-12-22 12:40:40 +0800

    spider

--- taobao_Spider/Channel_extract.py
+++ taobao_Spider/Channel_extract.py
@@ -1,16 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-start_urls = 'https://www.taobao.com/'
-headers = {
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Encoding':'gzip, deflate',
-    'Accept-Language':'en-US,en;q=0.5',
-    'Connection':'keep-alive',
-}
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+start_urls = 'https://www.taobao.com/tbhome/page/market-list'
+uaList = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586']
+dcap = dict(DesiredCapabilities.PHANTOMJS)
+dcap["phantomjs.page.settings.userAgent"] = (uaList)
+
 def Find_urls(url):
-    wb_data = requests.get(start_urls,headers=headers)
-    soup = BeautifulSoup(wb_data.text,'lxml')
-    print(soup)
+    driver = webdriver.PhantomJS(executable_path='/usr/bin/phantomjs',
+                                 desired_capabilities=dcap)
+    driver.get(start_urls)
+    wb_data = driver.page_source
+    soup = BeautifulSoup(wb_data)
+    links = soup.select('div.category-items > a')
+    for link in links:
+        href = link.get_text()
+        url = link.get('href')
+        data = {
+            'Name' : href,
+            'Link' : url,
+        }
+        print(data)
 Find_urls(start_urls)
 
