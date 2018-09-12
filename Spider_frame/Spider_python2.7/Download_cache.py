2bef3bbd42b611a94db49f755c8990d69a13ce63
Alexanderklau <429095816@qq.com>
2018-09-12 14:50:12 +0800

    delete

--- Spider_frame/Spider_python2.7/Download_cache.py
+++
@@ -1,5 +0,0 @@
-import urllib2
-class Downloader:
-    def __init__(self,delay=5,user_agent='wswp',proxies=None,
-                 num_retries=1,cache=None):
-        self.throttle = Throttle(delay)
--- Spider_frame/Spider_python2.7/Login_interface.py
+++
@@ -1,5 +0,0 @@
-import urllib2,urllib
-LOGIN_URL = 'http://www.zhihu.com/?next=%2Finbox%2F1723864600#signin'
-LOGIN_EMAIL = 'xxxxxxxxxxxx'
-LOGIN_PASSWORD = 'xxxxxxxxx'
-data = {}
