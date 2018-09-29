e64158cbfc089922be119e7d23ff22ddfb4240fa
Alexanderklau <429095816@qq.com>
2018-09-29 16:51:18 +0800

    修改

--- Code_crack/Image_code/simple_image_code.py
+++ Code_crack/Image_code/simple_image_code.py
@@ -3,15 +3,28 @@
 __author__ = 'lau.wenbo'
 
 """
-破解简单的图形验证码
+破解简单的图形验证码,拿到电话号码并且验证
 """
 
-import tesserocr
+import sys
+from pytesseract import *
+import requests
+import os
+import re
 from PIL import Image
-
-
-# image = Image.open('../Example_image/Image/simple3615.jpg')
-image = Image.open('1.jpg')
-result = tesserocr.image_to_text(image)
-print(result)
-
+from PIL import ImageEnhance
+import pkg_resources
+url = 'http://t.51chuli.com/contact/d91779cced1ade729yz45q72bn3o5o1z.gif'
+html = requests.get(url)
+with open('vercode.gif', 'wb') as f:
+    f.write(html.content)
+    f.flush()
+    os.fsync(f.fileno())
+if os.path.isfile('vercode.gif'):
+    image = Image.open('vercode.gif')
+    image = image.convert('L')
+    threshold = 250
+    initTable = lambda x:0 if x < threshold else 1
+    binaryImage = image.point(initTable, '1')
+    vcode = image_to_string(binaryImage, lang="eng", config='-psm 7')
+    print (vcode.replace(' ',''))
