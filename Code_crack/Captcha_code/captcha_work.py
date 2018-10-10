a9d1da5191d9e333574100f610e1211b637a293a
lau <429095816@qq.com>
2018-10-10 23:27:18 +0800

    验证码破解逻辑

---
+++ Code_crack/Captcha_code/captcha_work.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+__author__ = 'lau.wenbo'
+
+
+import pytesseract
+from PIL import Image
+
+
+image = Image.open("1.jpg")
+# 灰度处理，更加方便识别
+images = image.convert('L')
+table = []
+threshould = 80
+for i in range(256):
+    if i < threshould:
+        table.append(0)
+    else:
+        table.append(1)
+
+images = images.point(table, "1")
+result = pytesseract.image_to_string(images)
+print(result)
