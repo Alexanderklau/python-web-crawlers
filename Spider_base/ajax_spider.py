8180cab0d2229d8400a26ade8c8586621480496e
Alexanderklau <429095816@qq.com>
2019-03-24 16:22:07 +0800

    处理

---
+++ Spider_base/ajax_spider.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+
+from urllib.parse import urlencode
+import requests
+
+base_url = "https://m.weibo.cn/u/2830678474"
+
--- Spider_base/spider_base.py
+++ Spider_base/spider_base.py
@@ -1,3 +1,22 @@
 # coding: utf-8
 
 __author__ = "lau.wenbo"
+
+
+import requests
+
+r = requests.get("http://www.baidu.com/")
+# 下载图像
+
+headers = {
+    'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:63.0) Gecko/20100101 Firefox/63.0",
+
+}
+c = requests.get("https://www.zhihu.com/explore",headers=headers)
+print(c.text)
+z = requests.get("http://github.com/favicon.ico")
+with open("favicon.ico", "wb") as f:
+    f.write(z.content)
+
+
+
